d697d9de8784d24990acce9f6226e4b878d3465d
Willy Aguirre <marti1003@hotmail.com>
2013-06-02 11:47:33 -0500

    finish the chat

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var port = 3700;
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
     res.render("page");
 });
 
-app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function(socket){
